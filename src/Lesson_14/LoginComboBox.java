a2b6005420b4f297a81aa1707c2e8e080f88d5b1
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-12-02 19:32:35 +0200

    Update LoginComboBox.java file.

---
+++ src/Lesson_14/LoginComboBox.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template file, choose Settings | Editor | File and Code Templates
+ * and change the template in the editor.
+ */
+
+package Lesson_14;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+/**
+ * @author Ahmed Abuwarda, Date: Nov 27,2019.
+ */
+public class LoginComboBox extends Application {
+
+    private Stage window;
+
+    /**
+     * @param args the command line here.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        window = primaryStage;
+        window.setTitle("Ahmed");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(50, 40, 50, 40));
+        gridPane.setHgap(10);
+        gridPane.setVgap(8);
+
+        Label userLabel = new Label("Username:");
+        GridPane.setConstraints(userLabel, 0, 0);
+
+        TextField userInput = new TextField();
+        userInput.setPromptText("Username");
+        GridPane.setConstraints(userInput, 1, 0);
+
+        Label passLabel = new Label("Password");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        TextField passInput = new TextField();
+        passInput.setPromptText("Password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        ComboBox<String> comboBox = new ComboBox<>();
+        //ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        comboBox.getItems().addAll("Remember Me", "Don not Remember Me");
+        comboBox.setValue("What is your status?");
+        comboBox.setEditable(true);
+        GridPane.setConstraints(comboBox, 1, 2);
+        comboBox.setOnAction(e -> {
+            System.out.println(comboBox.getValue());
+        });
+
+        Button login = new Button("Login");
+        GridPane.setConstraints(login, 1, 3);
+
+        gridPane.getChildren().addAll(userLabel, userInput, passLabel, passInput, comboBox, login);
+
+        login.setOnAction(e -> System.out.println(isInt(userInput, passInput) + " " + comboBox.getValue()));
+
+        Scene scene = new Scene(gridPane, 300, 250);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+    private boolean isInt(TextField userInput, TextField passInput) {
+        if (userInput.getText().equals("aa") && passInput.getText().equals("aa")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
